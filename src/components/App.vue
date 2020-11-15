5b6dbdb799c4dcb3859e0cbb2cbe2a6e18f77745
SwipSwup <davidveigel.business@gmail.com>
2020-11-15 12:14:38 +0100

    add project files

---
+++ src/components/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <md-app
+      v-cloak
+      md-waterfall
+      md-mode="fixed"
+  >
+
+    <md-app-content>
+      <login button-text="Send it" email-placeholder="email" password-placeholder="password" title="Gamer login"></login>
+      <login button-text="Send it" email-placeholder="email" password-placeholder="password" title="Gamer login"></login>
+      <login button-text="Send it" email-placeholder="email" password-placeholder="password" title="Gamer login"></login>
+    </md-app-content>
+
+  </md-app>
+</template>
+
+<script>
+import Login from "@/components/login";
+export default {
+  name: 'App',
+  components: {Login},
+}
+</script>
+
+<style scoped>
+</style>
